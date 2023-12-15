e0e0df95d4e17e520758a2b5e446df38e643104e
Emmanuelito01 <emmatwice.exo@gmail.com>
2023-12-15 09:45:08 -0500

    commit emma

--- HTML/solicitudes.js
+++ HTML/solicitudes.js
@@ -6,7 +6,7 @@
 const mysql=require('mysql')
 //Crear
 function insertar(conex,data,callback){
-    let insertQ=insertar('{Nombre:'Nombre', Localidad:'Localidad',Dirección:'Dirección', Municipio:'Municipio'})' VALUES (?,?,?,?)
+    let insertQ=insertar(conex,{Nombre:'Nombre', Localidad:'Localidad',Dirección:'Dirección', Municipio:'Municipio'}), Values('?','?','?','?');
     console.log(insertar)
 let query=mysql.format(insertQ, [data.Nombre, data.Localidad, data.Dirección, data.Municipio])
 console.log(insertar)
